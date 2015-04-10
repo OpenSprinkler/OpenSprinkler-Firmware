c167ec69ee8ca1c1357ca77f3dbfefd8511c1028
Samer Albahra <salbahra@gmail.com>
2015-04-09 22:59:47 -0500

    Make OSPi the default for build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 echo "Building OpenSprinkler..."
 
-if [ "$1" == "ospi" ]; then
-	gcc -o OpenSprinkler -DOSPI main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp
+if [ "$1" == "demo" ]; then
+	g++ -o OpenSprinkler -DDEMO -m32 main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp
 elif [ "$1" == "osbo" ]; then
 	gcc -o OpenSprinkler -DOSBO main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp
 else
-	g++ -o OpenSprinkler -DDEMO -m32 main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp
+	gcc -o OpenSprinkler -DOSPI main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp
 fi
 
 if [ -f OpenSprinkler.launch ] && [ ! -f /etc/init.d/OpenSprinkler.sh ]; then
