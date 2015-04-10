a2ac3bb3ee444d69ab9dcf53cefaa2d57f1e5ff5
Samer Albahra <salbahra@gmail.com>
2015-04-09 19:50:50 -0500

    Add launch script by user Denny Fox

--- build_ospi.sh
+++ build_ospi.sh
@@ -1,1 +1,5 @@
 gcc -o OpenSprinkler -DOSPI main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp
+sed -i "s/\<\%OpenSprinkler_Path\%\>/${PWD##*/}/g" OpenSprinkler.sh
+mv OpenSprinkler.sh /etc/init.d/
+update-rc.d OpenSprinkler defaults
+/etc/init.d/OpenSprinkler start
