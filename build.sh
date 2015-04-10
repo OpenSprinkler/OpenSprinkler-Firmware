21d92360a1d7f6ed4bcba4bc5379ed01d2dd4459
Samer Albahra <salbahra@gmail.com>
2015-04-09 22:52:33 -0500

    Merge files autoLaunch.sh and build.sh

--- build.sh
+++ build.sh
@@ -10,4 +10,29 @@ else
 	g++ -o OpenSprinkler -DDEMO -m32 main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp
 fi
 
-./autoLaunch.sh
+if [ -f OpenSprinkler.launch ] && [ ! -f /etc/init.d/OpenSprinkler.sh ]; then
+	echo "Adding OpenSprinkler launch script..."
+
+	# Get current directory (binary location)
+	pushd `dirname $0` > /dev/null
+	DIR=`pwd`
+	popd > /dev/null
+
+	# Update binary location in start up script
+	sed -e 's,\_\_OpenSprinkler\_Path\_\_,'"$DIR"',g' OpenSprinkler.launch > OpenSprinkler.sh
+
+	# Make file executable
+	chmod +x OpenSprinkler.sh
+
+	# Move start up script to init.d directory
+	sudo mv OpenSprinkler.sh /etc/init.d/
+
+	# Add to auto-launch on system startup
+	sudo update-rc.d OpenSprinkler.sh defaults
+
+	# Start the deamon now
+	sudo /etc/init.d/OpenSprinkler.sh start
+
+fi
+
+echo "Done!"
