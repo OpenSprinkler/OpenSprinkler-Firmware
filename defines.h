e1bc80f7b22f191b58bc6e1529f85e836aa94aa7
Peter <peter_basham@yahoo.co.uk>
2016-01-23 20:39:00 +0000

    Enable GPIO stations for OSPi only

--- defines.h
+++ defines.h
@@ -317,6 +317,8 @@ typedef enum {
   #define PIN_BUTTON_2      24    // button 2
   #define PIN_BUTTON_3      25    // button 3
 
+  #define PIN_FREE_LIST		{5,6,7,8,9,10,11,12,13,16,18,19,20,21,23,24,25,26}
+  
   /** BBB pin defines */
   #elif defined(OSBO)
 
