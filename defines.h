c936c810bf9c692ee9e65dda7c498bb01da541d4
Peter Basham <peter_basham@yahoo.co.uk>
2016-04-02 14:19:46 +0000

    Bumped the firmware version to 2.1.7 to allow GPIO Stations and enhanced Zimmerman features to be released

--- defines.h
+++ defines.h
@@ -25,7 +25,7 @@
 #define _DEFINES_H
 
 /** Firmware version, hardware version, and maximal values */
-#define OS_FW_VERSION  216  // Firmware version: 216 means 2.1.6
+#define OS_FW_VERSION  217  // Firmware version: 217 means 2.1.7
                             // if this number is different from the one stored in non-volatile memory
                             // a device reset will be automatically triggered
 
