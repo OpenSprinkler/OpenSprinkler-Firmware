6cbfa522bc60d85762c90e7f7450664c78687506
ShawnHarte <shawn@veuphoria.com>
2016-05-19 20:26:31 -0600

    bump

---
+++ mainArduino/mainArduino.ino
@@ -0,0 +1,37 @@
+/* OpenSprinkler Unified (AVR/RPI/BBB/LINUX) Firmware
+ * Copyright (C) 2015 by Ray Wang (ray@opensprinkler.com)
+ *
+ * Main loop wrapper for Arduino
+ * Feb 2015 @ OpenSprinkler.com
+ *
+ * This file is part of the OpenSprinkler Firmware
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
+ 
+#include <OpenSprinkler.h>
+
+extern OpenSprinkler os;
+
+void do_setup();
+void do_loop();
+
+void setup() {
+  do_setup();
+}
+
+void loop() {
+  do_loop();
+}
