4eafe3afdd5fb5d7b661e98e970bcda15cb900fc
ray <rayshobbyshop@gmail.com>
2020-06-09 13:07:21 -0400

    improve build.sh by adding more echos, and adding -lmosquitto to osbo as well

--- build.sh
+++ build.sh
@@ -11,12 +11,19 @@ done
 echo "Building OpenSprinkler..."
 
 if [ "$1" == "demo" ]; then
+	echo "Installing required libraries..."
 	apt-get install -y libmosquitto-dev
+	echo "Compiling firmware..."
 	g++ -o OpenSprinkler -DDEMO -m32 main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp etherport.cpp mqtt.cpp -lpthread -lmosquitto
 elif [ "$1" == "osbo" ]; then
-	g++ -o OpenSprinkler -DOSBO main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp etherport.cpp mqtt.cpp -lpthread
-else
+	echo "Installing required libraries..."
 	apt-get install -y libmosquitto-dev
+	echo "Compiling firmware..."
+	g++ -o OpenSprinkler -DOSBO main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp etherport.cpp mqtt.cpp -lpthread -lmosquitto
+else
+	echo "Installing required libraries..."
+	apt-get install -y libmosquitto-dev
+	echo "Compiling firmware..."
 	g++ -o OpenSprinkler -DOSPI main.cpp OpenSprinkler.cpp program.cpp server.cpp utils.cpp weather.cpp gpio.cpp etherport.cpp mqtt.cpp -lpthread -lmosquitto
 fi
 
