5e27f4037ed1d083ffffa458104c9ea89e363f4f
Ray <rayshobbyshop@gmail.com>
2024-05-23 11:34:28 -0400

    merge master to rpi_gpiod branch

--- gpio.cpp
+++ gpio.cpp
@@ -177,6 +177,8 @@ byte digitalReadExt(byte pin) {
 
 #elif defined(OSPI) || defined(OSBO)
 
+#if !defined(LIBGPIOD)	// use classic sysfs
+
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
@@ -442,6 +444,138 @@ void attachInterrupt(int pin, const char* mode, void (*isr)(void)) {
 			delay(1) ;
 	pthread_mutex_unlock (&pinMutex) ;
 }
+#else // use GPIOD
+
+/**
+ * NEW GPIO Implementation for Raspberry Pi OS 12 (bookworm)
+ *
+ * Thanks to Jason Balonso
+ *  https://github.com/jbalonso/
+ *
+ *
+*/
+
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <poll.h>
+#include <pthread.h>
+#include <gpiod.h>
+
+#include "utils.h"
+
+#define BUFFER_MAX 64
+#define GPIO_MAX	 64
+
+// GPIO interfaces
+const char *gpio_chipname = "gpiochip0";
+const char *gpio_consumer = "opensprinkler";
+
+struct gpiod_chip *chip = NULL;
+struct gpiod_line* gpio_lines[] = {
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL,
+};
+
+int assert_gpiod_chip() {
+	if( !chip ) {
+		chip = gpiod_chip_open_by_name(gpio_chipname);
+		if( !chip ) {
+			DEBUG_PRINTLN("failed to open gpio chip");
+			return -1;
+		} else {
+			DEBUG_PRINTLN("gpio chip opened");
+			return 0;
+		}
+	}
+	return 0;
+}
+
+int assert_gpiod_line(int pin) {
+	if( !gpio_lines[pin] ) {
+		if( assert_gpiod_chip() ) { return -1; }
+		gpio_lines[pin] = gpiod_chip_get_line(chip, pin);
+		if( !gpio_lines[pin] ) {
+			DEBUG_PRINT("failed to open gpio line ");
+			DEBUG_PRINTLN(pin);
+			return -1;
+		} else {
+			DEBUG_PRINT("opened gpio line ");
+			DEBUG_PRINT(pin);
+			return 0;
+		}
+	}
+	return 0;
+}
+
+/** Set pin mode, in or out */
+void pinMode(int pin, byte mode) {
+	if( assert_gpiod_line(pin) ) { return; }
+	switch(mode) {
+		case INPUT:
+			gpiod_line_request_input(gpio_lines[pin], gpio_consumer);
+			break;
+		case INPUT_PULLUP:
+			gpiod_line_request_input_flags(gpio_lines[pin], gpio_consumer, GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_UP);
+			break;
+		case OUTPUT:
+			gpiod_line_request_output(gpio_lines[pin], gpio_consumer, LOW);
+			break;
+		default:
+			DEBUG_PRINTLN("invalid pin direction");
+			break;
+	}
+
+	return;
+}
+
+/** Read digital value */
+byte digitalRead(int pin) {
+	if( !gpio_lines[pin] ) {
+		DEBUG_PRINT("tried to read uninitialized pin ");
+		DEBUG_PRINTLN(pin);
+		return 0;
+	}
+	int val = gpiod_line_get_value(gpio_lines[pin]);
+	if( val < 0 ) {
+		DEBUG_PRINT("failed to read value on pin ");
+		DEBUG_PRINTLN(pin);
+		return 0;
+	}
+	return val;
+}
+
+/** Write digital value */
+void digitalWrite(int pin, byte value) {
+	if( !gpio_lines[pin] ) {
+		DEBUG_PRINT("tried to write uninitialized pin ");
+		DEBUG_PRINTLN(pin);
+		return;
+	}
+
+	int res;
+	res = gpiod_line_set_value(gpio_lines[pin], value);
+	if( res ) {
+		DEBUG_PRINT("failed to write value on pin ");
+		DEBUG_PRINTLN(pin);
+	}
+}
+
+void attachInterrupt(int pin, const char* mode, void (*isr)(void)) {}
+void gpio_write(int fd, byte value) {}
+int gpio_fd_open(int pin, int mode) {return 0;}
+void gpio_fd_close(int fd) {}
+
+#endif
+
 #else
 
 void pinMode(int pin, byte mode) {}
