fed839580dfd37506b570faa0d3239d4f0728b32
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2024-07-29 10:32:57 -0400

    switch to systemd to fix issue of Opensprinkler starting before wifi

--- build.sh
+++ build.sh
@@ -63,7 +63,7 @@ else
 	g++ -o OpenSprinkler -DOSPI $USEGPIO -DSMTP_OPENSSL -std=c++14 -include string.h main.cpp OpenSprinkler.cpp program.cpp opensprinkler_server.cpp utils.cpp weather.cpp gpio.cpp mqtt.cpp smtp.c -Iexternal/TinyWebsockets/tiny_websockets_lib/include $ws -Iexternal/OpenThings-Framework-Firmware-Library/ $otf -lpthread -lmosquitto -lssl -lcrypto $GPIOLIB
 fi
 
-if [ ! "$SILENT" = true ] && [ -f OpenSprinkler.launch ] && [ ! -f /etc/init.d/OpenSprinkler.sh ]; then
+if [ ! "$SILENT" = true ] && [ -f OpenSprinkler.service ] && [ -f startOpenSprinkler.sh ] && [ ! -f /etc/systemd/system/OpenSprinkler.service ]; then
 
 	read -p "Do you want to start OpenSprinkler on startup? " -n 1 -r
 	echo
@@ -72,28 +72,18 @@ if [ ! "$SILENT" = true ] && [ -f OpenSprinkler.launch ] && [ ! -f /etc/init.d/O
 		exit 0
 	fi
 
-	echo "Adding OpenSprinkler launch script..."
+	echo "Adding OpenSprinkler launch service..."
 
 	# Get current directory (binary location)
-	pushd `dirname $0` > /dev/null
-	DIR=`pwd`
+	pushd "$(dirname $0)" > /dev/null
+	DIR="$(pwd)"
 	popd > /dev/null
 
 	# Update binary location in start up script
-	sed -e 's,\_\_OpenSprinkler\_Path\_\_,'"$DIR"',g' OpenSprinkler.launch > OpenSprinkler.sh
+	sed -e 's,\_\_OpenSprinkler\_Path\_\_,'"$DIR"',g' OpenSprinkler.service > /etc/systemd/system/OpenSprinkler.service
 
 	# Make file executable
-	chmod +x OpenSprinkler.sh
-
-	# Move start up script to init.d directory
-	sudo mv OpenSprinkler.sh /etc/init.d/
-
-	# Add to auto-launch on system startup
-	sudo update-rc.d OpenSprinkler.sh defaults
-
-	# Start the deamon now
-	sudo /etc/init.d/OpenSprinkler.sh start
-
+	chmod +x startOpenSprinkler.sh
 fi
 
 echo "Done!"
