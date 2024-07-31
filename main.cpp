bc29201b403ef7ded989dc21402dd534068226e3
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2024-07-31 12:24:02 -0400

    disable printf buffering

--- main.cpp
+++ main.cpp
@@ -2016,6 +2016,8 @@ static void perform_ntp_sync() {
 
 #if !defined(ARDUINO) // main function for RPI/BBB
 int main(int argc, char *argv[]) {
+    // Disable buffering to work with systemctl journal
+    setvbuf(stdout, NULL, _IOLBF, 0);
 	printf("Starting OpenSprinkler\n");
 
 	int opt;
