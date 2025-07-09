591ff820605874a703b561cbe30d2befa9fa79f1
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2025-07-09 15:17:17 -0400

    fix interface again

--- analog.h
+++ analog.h
@@ -6,10 +6,10 @@
 class AnalogSensor {
 public:
   virtual ~AnalogSensor() = default;
-  virtual uint8_t pin_count();
-  virtual int16_t get_pin_value(uint8_t pin);
-  virtual float get_scale_constant(uint8_t pin);
-  virtual float get_scale_factor(uint8_t pin);
+  virtual uint8_t pin_count() = 0;
+  virtual int16_t get_pin_value(uint8_t pin) = 0;
+  virtual float get_scale_constant(uint8_t pin) = 0;
+  virtual float get_scale_factor(uint8_t pin) = 0;
 };
 
 #endif // ANALOG_H
