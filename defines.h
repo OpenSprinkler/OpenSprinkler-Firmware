4391cd7a0823dc3bb5cdc153ce4d50fc4b15aab7
Stephen Maurer <stephen.maurer@icloud.com>
2025-12-09 10:35:17 +1100

    Updated GPIO pins for OSPI to work wiht my relay board

--- defines.h
+++ defines.h
@@ -458,19 +458,19 @@ enum {
 #elif defined(OSPI) // for OSPi
 
 	#define OS_HW_VERSION    OSPI_HW_VERSION_BASE
-	#define PIN_SR_LATCH      22    // shift register latch pin
+	#define PIN_SR_LATCH      27    // shift register latch pin
 	#define PIN_SR_DATA       27    // shift register data pin
-	#define PIN_SR_DATA_ALT   21    // shift register data pin (alternative, for RPi 1 rev. 1 boards)
-	#define PIN_SR_CLOCK       4    // shift register clock pin
+	#define PIN_SR_DATA_ALT   27    // shift register data pin (alternative, for RPi 1 rev. 1 boards)
+	#define PIN_SR_CLOCK      17    // shift register clock pin
 	#define PIN_SR_OE         17    // shift register output enable pin
 	#define PIN_SENSOR1       14
-	#define PIN_SENSOR2       23
+	#define PIN_SENSOR2       14
 	#define PIN_RFTX          15    // RF transmitter pin
-	#define PIN_BUTTON_1      24    // button 1
-	#define PIN_BUTTON_2      18    // button 2
-	#define PIN_BUTTON_3      10    // button 3
+	#define PIN_BUTTON_1      15    // button 1
+	#define PIN_BUTTON_2      15    // button 2
+	#define PIN_BUTTON_3      15    // button 3
 
-	#define PIN_FREE_LIST       {5,6,7,8,9,11,12,13,16,19,20,21,23,25,26}  // free GPIO pins
+	#define PIN_FREE_LIST       {5,6,7,8,9,10,11,12,13,16,19,20,21,22,23,24,25,26}  // free GPIO pins
 	#define ETHER_BUFFER_SIZE   16384
 
 	#define SDA 0
